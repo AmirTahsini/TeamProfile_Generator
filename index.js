56d4f835a23bcb082e3337d51f94426aebad67d8
flipmode <tahsini.amir@gmail.com>
2023-02-19 21:12:27 -0800

    create js file in lib folder

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const Employee = require('Employee');
+const Manager = require('Manager');
+const Intern = require('Intern');
+const Engineer = reuqire('Engineer');
+const {writeFile} = require('fs').promises;
